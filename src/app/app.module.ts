a643f81711041cd3f63d7c09c2636dcbf739eecb
Risheik <50860661+Risheik@users.noreply.github.com>
2020-09-28 00:46:45 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, HostListener } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { CompanyModule } from './company/company.module';
+import { StudentComponent } from './student/student.component';
+import { MypipePipe } from './mypipe.pipe';
+import { StudentdetailsComponent } from './studentdetails/studentdetails.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { RouterModule } from '@angular/router';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BookService} from './book.service';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import {TestData} from './testdata';
+import {HttpClientModule} from '@angular/common/http';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    StudentComponent,
+    MypipePipe,
+    StudentdetailsComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CompanyModule,
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    InMemoryWebApiModule.forRoot(TestData),
+    AngularFireDatabaseModule 
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [BookService],
+  bootstrap: [AppComponent],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule
+  ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(){
+    console.log("hi,this is module");
+  }
+ }
